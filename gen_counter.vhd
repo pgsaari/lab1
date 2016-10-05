9eb6a988e0ca578001d32abdb5a53900e899b912
Paul <pgsaari@uwm.edu>
2016-10-04 20:13:42 -0500

    Add reset and set choose functionality

--- gen_counter.vhd
+++ gen_counter.vhd
@@ -5,12 +5,14 @@ USE ieee.numeric_std.all;
 entity gen_counter is
 generic (
 		wide :positive; -- how many bits is the counter
-		max :positive   -- what is the max value of the counter ( modulus )
+		max :positive;   -- what is the max value of the counter ( modulus )
+		code :positive := 3 -- code to identify between hours minutes and seconds (hms)
 		);
 port (
 		clk		:in	std_logic; -- system clock
 		data	:in std_logic_vector( wide-1 downto 0 ); -- data in for parallel load, use unsigned(data) to cast to unsigned
 		load	:in std_logic; -- signal to load data into i_count i_count <= unsigned(data);
+		setEnable : in std_logic_vector(1 downto 0); -- signal to pick between hms
 		enable	:in std_logic; -- clock enable
 		reset	:in std_logic; -- reset to zeros use i_count <= (others => '0' ) since size depends on generic
 		count	:out std_logic_vector( wide-1 downto 0 ); -- count out
@@ -27,9 +29,9 @@ begin
 cnt: process( clk ) begin
 	if ( rising_edge(clk) ) then
 		-- if-then-else statements to control the counter go here
-		if(reset = '1') then
+		if(reset = '1') and (unsigned(setEnable) = code) then -- reset button is pressed and the proper code is inputted
 			i_count <= (others => '0');
-		elsif(load = '1') then
+		elsif(load = '1') and (unsigned(setEnable) = code) then -- set button is pressed and proper code is inputted
 			i_count <= unsigned(data);
 		elsif(enable = '1') then
 			if(i_term = '0') then-- if we are not at the max value of the counter
